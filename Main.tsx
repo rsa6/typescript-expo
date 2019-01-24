a4f889707ac1c5fbb881c09372e79d4e2119e81d
HansRanda <rsa6@naver.com>
2019-01-25 05:30:57 +0900

    last

--- Main.tsx
+++ Main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, TouchableHighlight } from 'react-native';
 import styles from './styles';
 import { AppContext, AppContextInterface } from './Store';
-
 
 interface MainProps {
   context: any
@@ -33,14 +32,61 @@ class Main extends React.Component<MainProps, MainState> {
         <Text style={styles.textH1}>{this.state.title}</Text>
         <AppContext.Consumer>
           {(store: AppContextInterface) => 
-            <View>
+            <View style={styles.inContainer}>
               <Text style={styles.textH2}>{store.data}</Text>
               <Text style={styles.textH2}>{store.age}</Text>
             </View>
           }
         </AppContext.Consumer>
+
+        <MultiTap onPress={() => alert('double tap!')} numberOfTouches={2}>
+          {/* <TouchableHighlight onPress={() => alert('box tapped!')}> */}
+            <View style={styles.box} />
+          {/* </TouchableHighlight> */}
+        </MultiTap>
       </View>
     )
+  }
+}
+
+interface MultiTapProps {
+  numberOfTouches: number;
+  onPress: any;
+}
+
+class MultiTap extends React.Component<MultiTapProps, object> {
+  static defaultProps = {
+    onPress: () => null,
+    numberOfTouches: 2,
+  };
+
+  onMoveShouldSetResponder = (event: any) => {
+    console.log(event.nativeEvent.pageX);
+    return true;
+  }
+
+  onStartShouldSetResponder = (event: any) => {
+    if (event.nativeEvent.touches.length === this.props.numberOfTouches) {
+      return true;
+    }
+    return false;
+  };
+
+  onResponderRelease = (event: any) => {
+    this.props.onPress();
+    console.log(event.nativeEvent.locationX)
+  };
+
+  render() {
+    return (
+      <View
+        onMoveShouldSetResponder={this.onMoveShouldSetResponder}
+        onStartShouldSetResponder={this.onStartShouldSetResponder}
+        onResponderRelease={this.onResponderRelease}
+      >
+        {this.props.children}
+      </View>
+    );
   }
 }
 
